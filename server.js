4b720a89322a3102a885fd1697c674f8ba78f362
Locke Monstah <Mloughran87@gmail.com>
2019-01-08 01:19:55 -0500

    Errors solved, listening on 8080

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "bullelkcd19",
-  database: "bugerdb"
+  database: "burgerdb"
 });
 
 connection.connect(function(err) {
