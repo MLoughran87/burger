6e2a3aea6408d2953878a4b37e2aa3101cfda2ea
Locke Monstah <Mloughran87@gmail.com>
2019-01-08 01:08:52 -0500

    most orm completed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Onion Ranch', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Texas Burger', true);
